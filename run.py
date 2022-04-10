676e7a31e79f89b59f6f025a4806a93004425666
Daniel-S28 <80227692+Daniel-S28@users.noreply.github.com>
2022-04-10 17:40:40 -0500

    que pena

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 from clases.final import Final
 from clases.incial import Inicial
 from clases.juego import Juego
+from funcionalidades import io
 
 juego=Juego()
 
 if __name__=="__main__":
     juego=Juego()
     while(True):
-        print("Daza feo")
+        print("oe")
 
