b92595627492a7992dea20ea4a1f45e6d48b6989
Daniel-S28 <80227692+Daniel-S28@users.noreply.github.com>
2022-04-10 16:36:41 -0500

    daza gay

--- clases/final.py
+++ clases/final.py
@@ -12,7 +12,7 @@ class Final:
         elif self.cartas[-1][0] < carta[0] and carta[1] == self.pinta:    #Si la ultima carta de la torre es menor a la que se va a poner y es de la misma pinta que se appendee
             self.cartas.append(carta)
         else:
-            print("Movimiento invalido, respete hpta")
+            return False
     
 
         
