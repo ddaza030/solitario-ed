729f1f0be18fec189ffbbacde4c54e50ff7a97e7
Daniel-S28 <80227692+Daniel-S28@users.noreply.github.com>
2022-04-10 17:14:09 -0500

    ano

--- clases/final.py
+++ clases/final.py
@@ -14,6 +14,8 @@ class Final:
         else:
             return False
     
+    
+    
 
         
 
--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 from clases.final import Final
 from clases.incial import Inicial
 from clases.juego import Juego
+
+juego=Juego()
+
