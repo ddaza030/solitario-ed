34bfd152aec7107dcfadd2a7528d2f65d8e808a6
Juanes8a <80339256+Juanes8a@users.noreply.github.com>
2022-04-13 09:16:28 -0500

    Socrates

--- funcionalidades/io.py
+++ funcionalidades/io.py
@@ -1,23 +1,34 @@
-# Funcion pa printear
+#Funcion pa printear
+from clases.incial import Inicial
+from funcionalidades import funcion
+
 def printear():
     pass
 
 
-# Funcion para movimientos
-def movimientos():
-    movim = int(input("¿Qué movimiento deseas realizar?Ingresa solo el número:\n" +
-                      "1. Mover de columna A a B n (cantidad de cartas a mover)\n" +
-                      "2. Destapar cola de arraste\n" +
-                      "3. Reiniciar cola de arraste\n" +
-                      "4. Mover de cola de arraste a torre final X\n" +
-                      "5. Mover de cola de arraste a columna Y\n" +
-                      "6. Mover de columna Y a torre final X\n" +
-                      "7. Mover de torre final X a columna Y\n"))
+#Funcion para movimientos
+def movimientos(juego):
+    movim = int(input("¿Qué movimiento deseas realizar?Ingresa solo el número:\n"+
+                    "1. Mover de columna A a B n (cantidad de cartas a mover)\n"+
+                    "2. Destapar cola de arraste\n"+
+                    "3. Reiniciar cola de arraste\n"+
+                    "4. Mover de cola de arraste a torre final X\n"+
+                    "5. Mover de cola de arraste a columna Y\n"+
+                    "6. Mover de columna Y a torre final X\n"+
+                    "7. Mover de torre final X a columna Y\n"))
     if movim == 1:
-        cartas = tuple(input("Por favor ingresa los datos así: A B n\n" +
-                             "A:Corresponde a la columna origen\n" +
-                             "B:Corresponde a la columna destino\n" +
-                             "n:Corresponde a cantidad de cartas que se desplazarán\n").split())
+        a,b,n = tuple(input("Por favor ingresa los datos así: A B n\n"+
+                            "A:Corresponde a la columna origen\n"+
+                            "B:Corresponde a la columna destino\n"+
+                            "n:Corresponde a cantidad de cartas que se desplazarán\n").split())
+
+        verdad_absoluta = funcion.pasar_cartas(juego.inciales[a-1],juego.iniciales[b-1],n)
+        if verdad_absoluta:
+            printear()
+        else:
+            print("Movimiento inválido, intente de nuevo")
+            printear()
+            movimientos(juego)
 
     elif movim == 2:
         destapar()
@@ -40,20 +51,20 @@ def movimientos():
     elif movim == 6:
         pass
 
-    elif movim == 7:
+    elif movim == 7:            
         pass
 
 
 '''
 Ensayo
+n = [(9,1),(3,2),(2,4)]
 while True:
-    i=0
     if referencia_baraja[0] == n[0][0] and referencia_baraja[1] == n[0][1]:
         break
     else:
         e=n.popleft()
         n.append(e)
-        print(n)     
+     
 
 #print(n[0][1] == referencia_baraja[1])
 
