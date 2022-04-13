aa2c572d21aac21ab228e13dc6f5436dd4830c18
Daniel Daza <80338410+ddaza030@users.noreply.github.com>
2022-04-13 08:41:47 -0500

    Update io.py

--- funcionalidades/io.py
+++ funcionalidades/io.py
@@ -1,13 +1,13 @@
-#Funcion pa printear
+# Funcion pa printear
 def printear():
     pass
 
 
 def movimientos():
-    movim=input("Movimientos disponibles:\n"+
-    "1. Mover de columna A a B n (cantidad de cartas a mover)\n"+
-    "2. Destapar cola de arraste\n"+
-    "3. Reiniciar cola de arraste\n"+
-    "4. Mover de cola de arraste a torre final X\n"+
-    "5. Mover de cola de arraste a columna Y\n"+
-    "6. Mover de columna Y a torre final X")
+    movim = input("Movimientos disponibles:\n" +
+                  "1. Mover de columna A a B n (cantidad de cartas a mover)\n" +
+                  "2. Destapar cola de arraste\n" +
+                  "3. Reiniciar cola de arraste\n" +
+                  "4. Mover de cola de arraste a torre final X\n" +
+                  "5. Mover de cola de arraste a columna Y\n" +
+                  "6. Mover de columna Y a torre final X")
